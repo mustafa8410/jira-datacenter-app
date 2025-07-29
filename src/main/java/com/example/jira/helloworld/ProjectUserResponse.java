1b66ff05cbbf36e733de4ec247e387a281972740
Mustafa <mustafa841034@gmail.com>
2025-07-29 16:43:09 +0300

    some fixes after the dashboard ui update

--- src/main/java/com/example/jira/helloworld/ProjectUserResponse.java
+++ src/main/java/com/example/jira/helloworld/ProjectUserResponse.java
@@ -1,6 +1,7 @@
 package com.example.jira.helloworld;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ProjectUserResponse {
     private String projectKey;
@@ -42,4 +43,17 @@ public class ProjectUserResponse {
     public void setUserName(String userName) {
         this.userName = userName;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProjectUserResponse that = (ProjectUserResponse) o;
+        return Objects.equals(projectKey, that.projectKey); // Or include username if needed
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(projectKey); // Or include username if needed
+    }
+
 }
