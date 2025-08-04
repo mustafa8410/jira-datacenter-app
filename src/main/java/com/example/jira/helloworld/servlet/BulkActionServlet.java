39fc7145c3e50d1d2582d679ed4d30a1c2ddb469
Mustafa <mustafa841034@gmail.com>
2025-08-04 15:33:09 +0300

    bulk actions for license revocation and deactivating users are added

--- src/main/java/com/example/jira/helloworld/servlet/BulkActionServlet.java
+++ src/main/java/com/example/jira/helloworld/servlet/BulkActionServlet.java
@@ -1,26 +1,88 @@
 package com.example.jira.helloworld.servlet;
+import com.atlassian.crowd.embedded.api.Group;
+import com.atlassian.jira.bc.JiraServiceContext;
+import com.atlassian.jira.bc.JiraServiceContextImpl;
+import com.atlassian.jira.bc.user.search.UserSearchService;
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.exception.PermissionException;
+import com.atlassian.jira.exception.RemoveException;
+import com.atlassian.jira.security.groups.GroupManager;
+import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.user.util.UserUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/plugins/servlet/bulk-action")
 public class BulkActionServlet extends HttpServlet {
+
+    private final ApplicationUser adminUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser();
+    private final JiraServiceContext serviceContext = new JiraServiceContextImpl(adminUser);
+    private final GroupManager groupManager = ComponentAccessor.getComponent(GroupManager.class);
+    private final UserUtil userUtil = ComponentAccessor.getComponent(UserUtil.class);
+    private final UserSearchService userSearchService = ComponentAccessor.getComponent(UserSearchService.class);
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        String[] selectedUsers = req.getParameterValues("selectedUsers");
 
-        if (selectedUsers != null) {
-            for (String username : selectedUsers) {
-                System.out.println("İşlem yapılacak kullanıcı: " + username);
-                // TODO: Kullanıcıya ait işlem yap (örneğin lisans iptali)
+        if(!groupManager.isUserInGroup(adminUser, "jira-administrators")) {
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN, "You do not have permission to perform this action");
+            return;
+        }
+        List<String> selectedUsers = List.of(req.getParameterValues("selectedUsers"));
+        String action = req.getParameter("action");
+
+        if(!List.of("remove-group", "deactivate").contains(action)) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid action specified");
+            return;
+        }
+        if (selectedUsers.isEmpty()) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "No users selected for action");
+            return;
+        }
+        List<ApplicationUser> users = new ArrayList<>();
+        for(String username: selectedUsers) {
+            ApplicationUser user = userSearchService.getUserByName(serviceContext, username);
+            if (user != null) {
+                users.add(user);
+            }
+
+        }
+        if(action.equals("remove-group")) {
+            Group group = groupManager.getGroup("jira-software-users");
+            for(ApplicationUser currentUser: users) {
+                try {
+                    userUtil.removeUserFromGroup(group, currentUser);
+                } catch (PermissionException | RemoveException e) {
+                    resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to remove user from group: " + e.getMessage());
+                    return;
+                }
             }
         }
 
-        resp.setContentType("application/json");
-        resp.getWriter().write("{\"status\":\"success\"}");
+        if(action.equals("deactivate")) {
+            for(ApplicationUser currentUser: users) {
+                try {
+                    userUtil.removeUserFromGroups(groupManager.getGroupsForUser(currentUser.getUsername()), currentUser);
+                } catch (PermissionException | RemoveException e) {
+                    resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to deactivate user: " + e.getMessage());
+                    return;
+                }
+            }
+        }
+
+        resp.setContentType("text/html");
+        resp.getWriter().write("<html><body>Action " + action + " performed successfully on selected users.<br/>");
+        for(ApplicationUser user : users) {
+            resp.getWriter().write("User: " + user.getDisplayName() + "<br/>");
+        }
+        resp.getWriter().write("<a href=\"/jira/plugins/servlet//my-plugin-dashboard\">Go back to Dashboard</a></body></html>");
+
     }
 }
